7123fa66cf7c689b254427c87c96b8b78fa30eba
Mohammad Faraz <mohammadfaraz786nyc@gmail.com>
2026-06-21 03:15:13 +0530

    Add files via upload

---
+++ countdown.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  2 00:37:44 2026
+
+@author: MOHAMMAD FARAZ
+"""
+
+n = int(input("Enter the number:"))
+def countdown(n):
+    
+    if  n <= 10:
+        print("Blastoff")
+    else:
+        print(n)
+        countdown(n-1)
+        
+countdown(n)
